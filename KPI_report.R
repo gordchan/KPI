c6a2c6dba540d010387a0a7e9f1a3e8bffb0aa73
gordchan <gordon.chan@mensa.org.hk>
2015-08-26 03:30:30 +0000

    Fixed formula not recalculating

--- KPI_report.R
+++ KPI_report.R
@@ -13,68 +13,84 @@ require("dplyr")
 require("lubridate")
 
 source("read_xlsx.R")
+source("process_kpi.R")
 
 # Reporting period --------------------------------------------------------
 
-report.year <- "15"
-report.month <- paste(month(5, label = TRUE, abbr = TRUE), report.year, sep = "")
-    report.month_ <- paste(" ", report.month, sep = "") ## Keep leading zero to keep as text in excel
-prev.month <- paste(month(5-1, label = TRUE, abbr = TRUE), report.year, sep = "")
-report.period <- paste(prev.month, report.month, sep = "-")
+to.YY <- 15 ## Input from func
+    from.YY <- to.YY - 1
+
+to.month <- 5 ## Input from func
+    to.Mmm <- month(to.month, label = TRUE, abbr = TRUE)
+    from.Mnn <- month(to.month + 1, label = TRUE, abbr = TRUE)
+
+to.MmmYY <- paste(to.Mmm , to.YY, sep = "")
+    to.MmmYY_ <- paste(" ", to.MmmYY, sep = "") # Keep leading zero for use as text in Excel
+    prev.MmmYY <- paste(to.Mmm, from.YY, sep = "")
+    
+from.MmmYY <- paste(from.Mnn, from.YY, sep = "")
+
+report.period <- paste(from.MmmYY, to.MmmYY, sep = "-")
 
 # Create filepath table ----------------------------------------------------------
 
 KPI_files <- 
     data.frame(
-        tempnames = list.files("template", pattern = "(\\.xls$)", full.names = FALSE),
-        temppaths = list.files("template", pattern = "(\\.xls$)", full.names = TRUE), 
+        temp.names = list.files("template", pattern = "(\\.xls$)", full.names = FALSE),
+        temp.paths = list.files("template", pattern = "(\\.xls$)", full.names = TRUE), 
     stringsAsFactors = FALSE)
 
 KPI_files <- KPI_files %>%
-    mutate(filepaths = gsub("(\\.xls$)", paste(report.month_, ".xls", sep = ""), temppaths)) %>%
-        mutate(filepaths = gsub("(^template)", "report", filepaths)) %>%
-            arrange(desc(tempnames))
+    mutate(file.paths = gsub("(\\.xls$)", paste(to.MmmYY_, ".xls", sep = ""), temp.paths)) %>%
+        mutate(file.paths = gsub("(^template)", "report", file.paths)) %>%
+            arrange(desc(temp.names))
 
 # Copy template for use as blank report -------------------------------------------
 
 for (i in 1:nrow(KPI_files)){
-    file.copy(from = KPI_files$temppaths[i],
-              to = KPI_files$filepaths[i],
+    file.copy(from = KPI_files$temp.paths[i],
+              to = KPI_files$file.paths[i],
               overwrite = TRUE,
               copy.mode = TRUE, copy.date = FALSE)
 }
 
 # Loading xls template ------------------------------------------------------
 
-as.KPI <- loadWorkbook(KPI_files$temppaths[1])
+as.KPI <- loadWorkbook(KPI_files$temp.paths[1])
     sheets.KPI <- getSheets(as.KPI)
 
-# as.SOP <- loadWorkbook(KPI_files$temppaths[2])
+# as.SOP <- loadWorkbook(KPI_files$temp.paths[2])
 #     sheets.SOP <- getSheets(as.SOP)
 #     
-# as.TRE <- loadWorkbook(KPI_files$temppaths[3])
+# as.TRE <- loadWorkbook(KPI_files$temp.paths[3])
 #     sheets.TRE <- getSheets(as.TRE)
 
 
 # Load and prepare KPI source data -----------------------------------
 
-    source("process_kpi.R")
-    
+kpi.1.c <- kpi.1(to.MmmYY)
+kpi.1.t <- kpi.t("kpi.1")
+kpi.1.p <- kpi.1(prev.MmmYY)
+
 # Update reporting month and period -----------------------------------
 
-reporting <- CellBlock(sheets.KPI$source, 1, 2, 2, 1)
+reporting.dates <- CellBlock(sheets.KPI$source, 1, 2, 2, 1)
 
-    CB.setMatrixData(reporting, as.matrix(c(report.month, report.period)), 1, 1)
+    CB.setMatrixData(reporting.dates, as.matrix(c(to.MmmYY_, report.period)), 1, 1)
 
 # Update A&E wait time -----------------------------------------------
 
-addDataFrame(AE_WT.prod, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=3, startColumn=3)
-addDataFrame(AE_WT.prod, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=3, startColumn=13)
-addDataFrame(AE_WT.prod, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=3, startColumn=23)
+addDataFrame(kpi.1.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=3, startColumn=3)
+addDataFrame(kpi.1.t, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=3, startColumn=13)
+addDataFrame(kpi.1.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=3, startColumn=22)
 
 # Saving xls reports ----------------------------------------------------
 
-    saveWorkbook(as.KPI, KPI_files$filepaths[1])
-#     saveWorkbook(as.SOP, KPI_files$filepaths[2])
-#     saveWorkbook(as.TRE, KPI_files$filepaths[3])
+as.KPI$setForceFormulaRecalculation(TRUE)
+# as.SOP$setForceFormulaRecalculation(TRUE)
+# as.TRE$setForceFormulaRecalculation(TRUE)
+
+    saveWorkbook(as.KPI, KPI_files$file.paths[1])
+#     saveWorkbook(as.SOP, KPI_files$file.paths[2])
+#     saveWorkbook(as.TRE, KPI_files$file.paths[3])
     
