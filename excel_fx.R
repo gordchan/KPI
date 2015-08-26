5c5ad837b7f6fe93f810900d094de7be9c5cdc62
gordchan <gordon.chan@mensa.org.hk>
2015-08-26 23:12:49 +0000

    update

--- excel_fx.R
+++ excel_fx.R
@@ -2,6 +2,28 @@
 require("dplyr")
 
 hosp <- c("CMC", "KCH", "KWH", "NLTH", "OLMH", "PMH", "WTSH", "YCH", "KWC")
+
+
+# S, W ----------------------------------------------------------------
+
+l <- which(LETTERS=="R")
+L <- LETTERS[l]
+Ln <- LETTERS[l+9]
+
+t.fx <- '=source!'
+fx <- data.frame(t = LETTERS[1:25], stringsAsFactors = FALSE)
+
+for (i in 1:25){
+        fx$t[i] <- paste(t.fx, LETTERS[l], 3+i, sep = "")
+}
+
+fx <- fx %>% mutate(py = gsub(LETTERS[l], LETTERS[l+9], t))
+
+write.csv(fx$t, paste("SW/", L, ".csv", sep = ""), row.names = FALSE, quote = FALSE)
+write.csv(fx$py, paste("SW/", Ln, ".csv", sep = ""), row.names = FALSE, quote = FALSE)
+
+#  T, X, AB ---------------------------------------------------------------
+
 t.fx <- '=IF(G10<>"N.A.", G10-S10, "N.A.")'
 fx <- data.frame(hosp, tv = LETTERS[1:9], stringsAsFactors = FALSE)
 l <- which(LETTERS=="G")
