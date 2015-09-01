bee3fd336a81f521fbe0b3b5c93970dfd120ad52
gordchan <gordon.chan@mensa.org.hk>
2015-09-01 12:31:12 +0000

    all kpi items completed

--- KPI_report.R
+++ KPI_report.R
@@ -99,10 +99,10 @@ as.KPI <- loadWorkbook(KPI_files$temp.paths[1])
         kpi.2.c <- kpi.2(to.MmmYY)
         kpi.2.p <- kpi.2(prev.to.MmmYY)
     # kpi.3
-        # kpi.3.1.c <- kpi.3.1(to.MmmYY)
-        # kpi.3.1.p <- kpi.3.1(prev.to.MmmYY)
-        # kpi.3.2.c <- kpi.3.2(to.MmmYY)
-        # kpi.3.2.p <- kpi.3.2(prev.to.MmmYY)
+        kpi.3.1.c <- kpi.3("kpi.3.1", to.MmmYY)
+        kpi.3.1.p <- kpi.3("kpi.3.1", prev.to.MmmYY)
+        kpi.3.2.c <- kpi.3("kpi.3.2", to.MmmYY)
+        kpi.3.2.p <- kpi.3("kpi.3.2", prev.to.MmmYY)
         kpi.3.3.c <- kpi.3.3(to.MmmYY)
         kpi.3.3.p <- kpi.3.3(prev.to.MmmYY)
         kpi.3.4.c <- kpi.10(to.MmmYY)
@@ -127,8 +127,14 @@ as.KPI <- loadWorkbook(KPI_files$temp.paths[1])
         kpi.6.2.c <- kpi.6("kpi.6.2", to.MmmYY)
         kpi.6.2.p <- kpi.6("kpi.6.2", prev.to.MmmYY)
     # kpi.7
-        # kpi.7.c <- kpi.7(to.MmmYY)
-        # kpi.7.p <- kpi.7(prev.to.MmmYY)
+        kpi.7.1.c <- kpi.7("kpi.7.1", to.MmmYY)
+        kpi.7.1.p <- kpi.7("kpi.7.1", prev.to.MmmYY)
+        kpi.7.2.c <- kpi.7("kpi.7.2", to.MmmYY)
+        kpi.7.2.p <- kpi.7("kpi.7.2", prev.to.MmmYY)
+        kpi.7.3.c <- kpi.7("kpi.7.3", to.MmmYY)
+        kpi.7.3.p <- kpi.7("kpi.7.3", prev.to.MmmYY)
+        kpi.7.4.c <- kpi.7("kpi.7.4", to.MmmYY)
+        kpi.7.4.p <- kpi.7("kpi.7.4", prev.to.MmmYY)
 
 # Update reporting month and period -----------------------------------
 
@@ -149,13 +155,13 @@ as.KPI <- loadWorkbook(KPI_files$temp.paths[1])
         
         addDataFrame(kpi.2.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=8, startColumn=22)
     # kpi.3
-        # addDataFrame(kpi.3.1.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=13, startColumn=3)
-        # addDataFrame(kpi.3.2.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=14, startColumn=3)
+        addDataFrame(kpi.3.1.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=13, startColumn=3)
+        addDataFrame(kpi.3.2.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=14, startColumn=3)
         addDataFrame(kpi.3.3.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=15, startColumn=3)
         addDataFrame(kpi.3.4.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=16, startColumn=3)
         
-        # addDataFrame(kpi.3.1.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=13, startColumn=22)
-        # addDataFrame(kpi.3.2.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=14, startColumn=22)
+        addDataFrame(kpi.3.1.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=13, startColumn=22)
+        addDataFrame(kpi.3.2.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=14, startColumn=22)
         addDataFrame(kpi.3.3.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=15, startColumn=22)
         addDataFrame(kpi.3.4.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=16, startColumn=22)
     # kpi.4
@@ -181,9 +187,15 @@ as.KPI <- loadWorkbook(KPI_files$temp.paths[1])
         addDataFrame(kpi.6.1.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=23, startColumn=22)
         addDataFrame(kpi.6.2.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=24, startColumn=22)
     # kpi.7
-        # addDataFrame(kpi.7.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=25, startColumn=3)
+        addDataFrame(kpi.7.1.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=25, startColumn=3)
+        addDataFrame(kpi.7.2.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=26, startColumn=3)
+        addDataFrame(kpi.7.3.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=27, startColumn=3)
+        addDataFrame(kpi.7.4.c, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=28, startColumn=3)
 
-        # addDataFrame(kpi.7.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=25, startColumn=22)
+        addDataFrame(kpi.7.1.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=25, startColumn=22)
+        addDataFrame(kpi.7.2.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=26, startColumn=22)
+        addDataFrame(kpi.7.3.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=27, startColumn=22)
+        addDataFrame(kpi.7.4.p, sheets.KPI$source, col.names=FALSE, row.names=FALSE, startRow=28, startColumn=22)
 
 # Saving xls reports ----------------------------------------------------
 
